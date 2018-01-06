3e813658326b9bc4bbfb3b1af38b9596f3ee8fb9
Pyxis <pyxis@uzuki.live>
2018-01-06 16:36:00 +0900

    implement ImageDetailsActivity

--- library/src/main/java/pyxis/uzuki/live/naraeimagepicker/activity/ImageDetailsActivity.kt
+++ library/src/main/java/pyxis/uzuki/live/naraeimagepicker/activity/ImageDetailsActivity.kt
@@ -1,10 +1,14 @@
 package pyxis.uzuki.live.naraeimagepicker.activity
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_image_details.*
 import pyxis.uzuki.live.naraeimagepicker.Constants
+import pyxis.uzuki.live.naraeimagepicker.R
 
 /**
  * NaraeImagePicker
@@ -17,11 +21,34 @@ import pyxis.uzuki.live.naraeimagepicker.Constants
 class ImageDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_details)
 
         val path = intent.getStringExtra(Constants.EXTRA_DETAIL_IMAGE)
+
+        supportActionBar?.apply {
+            setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(this@ImageDetailsActivity, android.R.color.black)))
+            title = ""
+            setDisplayHomeAsUpEnabled(true)
+            hide()
+        }
+
+        photoView.setOnViewTapListener { view, x, y ->
+            if (supportActionBar?.isShowing == true) {
+                supportActionBar?.hide()
+            } else {
+                supportActionBar?.show()
+            }
+        }
 
         Glide.with(this)
                 .load(path)
                 .into(photoView)
+    }
+
+    override fun onOptionsItemSelected(menuItem: MenuItem): Boolean {
+        when {
+            menuItem.itemId == android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(menuItem)
     }
 }
